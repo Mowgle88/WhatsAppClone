2c2701c1e3cb853e99903c7425e38e66c3c220de
Dmitry Ilyin <mowgle>
2023-03-27 23:05:41 +0300

    fix: resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Text, StatusBar, Button, Alert } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import SplashScreen from "react-native-splash-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ChatListScreen from "./screens/ChatListScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  const pressHandler = () => {
-    Alert.alert("WhatsAppClone");
-  };
   return (
     <SafeAreaProvider style={styles.container}>
-      <SafeAreaView>
-        <StatusBar barStyle="default" backgroundColor="#3498db" />
-        <Text style={styles.label}>WhatsAppClone! </Text>
-        <Button title="Click me" onPress={pressHandler} />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Chat List" component={ChatListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
@@ -26,8 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#3498db",
-    alignItems: "center",
-    justifyContent: "center",
   },
   label: {
     color: "white",
