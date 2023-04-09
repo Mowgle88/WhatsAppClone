8b93888538479863a22dd527e43329e70a958a70
Dmitry Ilyin <mowgle>
2023-04-10 01:36:17 +0300

    feat: add buttons and input to ChatScreen

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "../constants/colors";
 
 const ChatScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ImageBackground
         source={require("../assets/images/droplet.jpeg")}
         resizeMode="cover"
         style={styles.backgroundImage}
       />
-    </View>
+      <View style={styles.inputContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Icon name="add-outline" size={24} color={colors.blue} />
+        </TouchableOpacity>
+        <TextInput style={styles.input} />
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Icon name="camera-outline" size={24} color={colors.blue} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -19,6 +37,25 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    height: 50,
+  },
+  button: {
+    width: 36,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: colors.lightGrey,
+    marginHorizontal: 16,
+    paddingHorizontal: 12,
   },
 });
 
