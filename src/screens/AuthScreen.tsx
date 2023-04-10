645613321693c5bf2ff5b98a32e5e710ee75b08d
Dmitry Ilyin <mowgle>
2023-04-11 01:27:26 +0300

    feat: add AuthScreen

---
+++ src/screens/AuthScreen.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import PageContainer from "../components/PageContainer";
+
+const AuthScreen: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <PageContainer>
+        <Text>AuthScreen</Text>
+      </PageContainer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default AuthScreen;
