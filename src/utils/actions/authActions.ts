6cbe634d38d6998f4f6e6c87c4880693ffc9523b
Dmitry Ilyin <mowgle>
2023-04-17 00:05:32 +0300

    feat: add sending user data to the database

--- src/utils/actions/authActions.ts
+++ src/utils/actions/authActions.ts
@@ -1,6 +1,7 @@
 import { getFirebaseApp } from "../firebaseHelper";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { FirebaseError } from "@firebase/util";
+import { getDatabase, set, ref, child } from "firebase/database";
 
 export const signUp = async (
   firstName: string,
@@ -12,7 +13,15 @@ export const signUp = async (
   const auth = getAuth(app);
 
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const { uid } = response.user;
+
+    await createUser(firstName, lastName, email, uid);
   } catch (error: unknown) {
     if (error instanceof FirebaseError) {
       const errorCode = error.code;
@@ -26,4 +35,26 @@ export const signUp = async (
       throw new Error(message);
     }
   }
+};
+
+const createUser = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  userId: string
+) => {
+  const fullName = `${firstName} ${lastName}`.toLowerCase();
+  const userData = {
+    firstName,
+    lastName,
+    fullName,
+    email,
+    userId,
+    signUpDate: new Date().toISOString(),
+  };
+
+  const dbRef = ref(getDatabase());
+  const childRef = child(dbRef, `users/${userId}`);
+  await set(childRef, userData);
+  return userData;
 };
