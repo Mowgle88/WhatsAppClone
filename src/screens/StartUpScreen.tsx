e9828c421dea836ad9a437e7835449950aea706d
Dmitry Ilyin <mowgle>
2023-04-30 17:58:46 +0300

    fix: fix dispatchies on the StartUpScreen

--- src/screens/StartUpScreen.tsx
+++ src/screens/StartUpScreen.tsx
@@ -1,39 +1,38 @@
 import React, { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import colors from "../constants/colors";
 import commonStyles from "../constants/commonStyles";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useDispatch } from "react-redux";
 import { authenticate, setDidTryAutoLogin } from "../store/authSlice";
+import { useAppDispatch } from "../store/hooks";
 import { getUserData } from "../utils/actions/userActions";
 
 const StartUpScreen = () => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     const tryLogin = async () => {
       const storedAuthInfo = await AsyncStorage.getItem("userData");
 
       if (!storedAuthInfo) {
-        dispatch(setDidTryAutoLogin);
+        dispatch(setDidTryAutoLogin());
         return;
       }
 
       const parsedData = JSON.parse(storedAuthInfo);
       const { token, userId, expiryData: expiryDataString } = parsedData;
-      const expiryData = new Date(expiryDataString);
 
+      const expiryData = new Date(expiryDataString);
       if (expiryData <= new Date() || !token || !userId) {
-        dispatch(setDidTryAutoLogin);
+        dispatch(setDidTryAutoLogin());
         return;
       }
 
       const userData = await getUserData(userId);
-
       dispatch(authenticate({ token, userData }));
     };
     tryLogin();
-  }, []);
+  }, [dispatch]);
 
   return (
     <View style={commonStyles.center}>
